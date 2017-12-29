f89b064ebf0a47b6024afc7aeb4493af604d678d
Daniel Morales <daniminas@gmail.com>
2017-12-29 13:48:29 -0300

    Rewriting

--- src/dmuploader.js
+++ src/dmuploader.js
@@ -19,18 +19,17 @@
 
   // These are the plugin defaults values
   var defaults = {
+    queue: true,
+    auto: true,
     url: document.URL,
     method: 'POST',
     extraData: {},
     headers: {},
+    dataType: null,
+    fieldName: 'file',
     maxFileSize: null,
-    maxFiles: null,
     allowedTypes: '*',
     extFilter: null,
-    dataType: null,
-    fileName: 'file',
-    auto: true,
-    skipChecks: false,
     onInit: function(){},
     onFallbackMode: function(message) {},
     onNewFile: function(id, file){},
@@ -42,25 +41,9 @@
     onFileTypeError: function(file){},
     onFileSizeError: function(file){},
     onFileExtError: function(file){},
-    onFilesMaxError: function(file){},
     onDragOver: function(){},
     onDragLeave: function(){},
     onDrop: function(){}
-  };
-
-  var DmUploader = function(element, options)
-  {
-    this.element = $(element);
-
-    this.settings = $.extend({}, defaults, options);
-
-    if(!this.settings.skipChecks && !this.checkBrowser()){
-      return false;
-    }
-
-    this.init();
-
-    return true;
   };
   
   var DmUploaderFile = function(file)
@@ -71,254 +54,55 @@
 
     this.status = FileStatus.PENDING;
 
-    this.id = -1;
+    // The file id doesnt have to bo that special.... or not?
+    this.id = Date.now().toString(36).substr(0, 8);
   };
 
-  DmUploaderFile.prototype.setId = function(id)
+  DmUploaderFile.prototype.upload = function(widget)
   {
-    this.id = id;
+    return true;
   };
 
   DmUploaderFile.prototype.cancel = function()
   {
-    switch (this.status){
-      case FileStatus.PENDING:
-
-        this.status = FileStatus.CANCELLED;
-        break;
-      case FileStatus.UPLOADING:
-
-        this.status = FileStatus.CANCELLED;
-        this.jqXHR.abort();
-        break;
-      default:
-        return false;
-    }
-
     return true;
   };
 
-  DmUploaderFile.prototype.upload = function(widget, single)
+  var DmUploader = function(element, options)
   {
-    var file = this;
+    this.element = $(element);
+    this.settings = $.extend({}, defaults, options);
 
-    // Cancelled by the user?
-    if (!single && (file.status == FileStatus.CANCELLED)){
-      widget.processQueue();
+    this.queue = [];
+    this.queuePos = -1;
+    this.queueRunning = false;
 
-      return false;
-    }
+    this.init();
 
-    // Uploading, or completed files...
-    if (file.status == FileStatus.UPLOADING ||
-        file.status == FileStatus.COMPLETED){
-      if (!single){
-        widget.processQueue();
-      }
-
-      return false;
-    }
-
-    // Form Data
-    var fd = new FormData();
-    fd.append(widget.settings.fileName, file.file);
-
-    // Return from client function (default === undefined)
-    var can_continue = widget.settings.onBeforeUpload.call(widget.element, file.id);
-
-    // If the client function doesn't return FALSE then continue
-    if( false === can_continue ) {
-      return;
-    }
-
-    // Append extra Form Data
-    var customData = widget.settings.extraData;
-    if (typeof(widget.settings.extraData) === "function"){
-      customData = widget.settings.extraData.call(widget.element, file.id);
-    }
-    $.each(customData, function(exKey, exVal){
-      fd.append(exKey, exVal);
-    });
-
-    if (!single){
-      widget.queueRunning = true;
-    }
-
-    file.status = FileStatus.UPLOADING;
-
-    // Ajax Submit
-    file.jqXHR = $.ajax({
-      url: widget.settings.url,
-      type: widget.settings.method,
-      dataType: widget.settings.dataType,
-      data: fd,
-      headers: widget.settings.headers,
-      cache: false,
-      contentType: false,
-      processData: false,
-      forceSync: false,
-      xhr: function(){
-        var xhrobj = $.ajaxSettings.xhr();
-        if(xhrobj.upload){
-          xhrobj.upload.addEventListener('progress', function(event) {
-            var percent = 0;
-            var position = event.loaded || event.position;
-            var total = event.total || event.totalSize;
-            if(event.lengthComputable){
-              percent = Math.ceil(position / total * 100);
-            }
-
-            widget.settings.onUploadProgress.call(widget.element, file.id, percent);
-          }, false);
-        }
-
-        return xhrobj;
-      },
-      success: function (data){
-        file.status = FileStatus.COMPLETED;
-        widget.settings.onUploadSuccess.call(widget.element, file.id, data);
-      },
-      error: function (xhr, status, errMsg){
-        // If the status is: cancelled (by the user) don't invoke the error callback
-        if (file.status != FileStatus.CANCELLED){
-          file.status = FileStatus.FAILED;
-          widget.settings.onUploadError.call(widget.element, file.id, errMsg);
-        }
-      },
-      complete: function(){
-        if (widget.settings.auto){
-          widget.processQueue();
-        }
-      }
-    });
-
-    return true;
-  };
-
-  // Public API methods
-  DmUploader.prototype.methods = {
-    cancel: function(id) {
-      /* Stops(if uploading) and Remove the upload from Queue */
-      if (id > (this.queue.length - 1)){
-        return false;
-      }
-
-      return this.queue[id].cancel();
-    },
-    cancelAll: function() {
-      /* Same as cancel, but for all pending uploads */
-      var nCancels = 0;
-
-      $.each(this.queue, function(id, file){
-        if (file.cancel()){
-          nCancels++;
-        }
-      });
-
-      return nCancels;
-    },
-    start: function(id) {
-      /* Start or re-try the upload */
-      if (id > (this.queue.length - 1)){
-        return false;
-      }
-
-      // If we are using a File queue only allow to start (or retry)
-      // files that that were cancelled or failed (NOT files that are 'next' in
-      // our queue.
-      if (this.settings.auto && (id >= this.queuePos)){
-        return false;
-      }
-
-      return this.queue[id].upload(this, true);
-    },
-    supportsProgress: function() {
-      var xhrobj = $.ajaxSettings.xhr();
-      
-      return (xhrobj.upload !== undefined);
-    },
-    supportsDND: function() {
-      return (this.checkEvent('drop', this.element) && !this.checkEvent('dragstart', this.element));
-    },
-    reset: function() {
-      console.log('TODO: Reset plugin');
-
-      return true;
-    }
-  };
-
-  DmUploader.prototype.checkBrowser = function()
-  {
-    if(window.FormData === undefined){
-      this.settings.onFallbackMode.call(this.element, 'Browser doesn\'t support Form API');
-
-      return false;
-    }
-
-    if(this.element.find('input[type=file]').length > 0){
-      return true;
-    }
-
-    if (!this.checkEvent('drop', this.element) || !this.checkEvent('dragstart', this.element)){
-      this.settings.onFallbackMode.call(this.element, 'Browser doesn\'t support Ajax Drag and Drop');
-
-      return false;
-    }
-
-    return true;
-  };
-
-  DmUploader.prototype.checkEvent = function(eventName, element)
-  {
-    element = element || document.createElement('div');
-    eventName = 'on' + eventName;
-
-    var isSupported = eventName in element;
-
-    if(!isSupported){
-      if(!element.setAttribute){
-        element = document.createElement('div');
-      }
-      if(element.setAttribute && element.removeAttribute){
-        element.setAttribute(eventName, '');
-        isSupported = typeof element[eventName] == 'function';
-
-        if(typeof element[eventName] != 'undefined'){
-          element[eventName] = undefined;
-        }
-        element.removeAttribute(eventName);
-      }
-    }
-
-    element = null;
-    return isSupported;
+    return this;
   };
 
   DmUploader.prototype.init = function()
   {
     var widget = this;
 
-    widget.queue = [];
-    widget.queuePos = -1;
-    widget.queueRunning = false;
+    //-- Optional File input to make a clickable area
+    widget.element.find('input[type=file]').on('change', function(evt){
+      var files = evt.target.files;
+
+      widget.addFiles(files);
+
+      $(this).val('');
+    });
 
     // -- Drag and drop events
     widget.element.on('drop', function (evt){
       evt.preventDefault();
       var files = evt.originalEvent.dataTransfer.files;
 
-      widget.queueFiles(files);
+      widget.addFiles(files);
 
       widget.settings.onDrop.call(this.element);
-    });
-
-    //-- Optional File input to make a clickable area
-    widget.element.find('input[type=file]').on('change', function(evt){
-      var files = evt.target.files;
-
-      widget.queueFiles(files);
-
-      $(this).val('');
     });
 
     //-- These two events/callbacks are onlt to maybe do some fancy visual stuff
@@ -332,18 +116,20 @@
 
     // We good to go, tell them!
     this.settings.onInit.call(this.element);
+
+    return this;
   };
 
-  DmUploader.prototype.queueFiles = function(files)
+  DmUploader.prototype.addFiles = function(files)
   {
-    var j = this.queue.length;
+    var nFiles = 0;
 
     for (var i= 0; i < files.length; i++)
     {
       var file = files[i];
 
       // Check file size
-      if((this.settings.maxFileSize !== null) &&
+      if((this.settings.maxFileSize > 0) &&
           (file.size > this.settings.maxFileSize)){
 
         this.settings.onFileSizeError.call(this.element, file);
@@ -373,43 +159,42 @@
         }
       }
 
-      // Check max files
-      if(this.settings.maxFiles !== null) {
-        if(this.queue.length >= this.settings.maxFiles) {
-          this.settings.onFilesMaxError.call(this.element, file);
+      var fileObj = new DmUploaderFile(file);
+      var can_continue = this.settings.onNewFile.call(this.element, fileObj.id, file);
 
-          continue;
-        }
+      // If the callback returns false file will not be processed. This may allow some customization
+      if (can_continue === false) {
+        return;
       }
 
-      var fileObj = new DmUploaderFile(file);
+      if(this.settings.auto && !this.settings.queue){
+        fileObj.upload(this);
+      }
+
       this.queue.push(fileObj);
-
-      var index = this.queue.length - 1;
-      fileObj.setId(index);
-
-      this.settings.onNewFile.call(this.element, index, file);
+      
+      nFiles++;
     }
 
-    // Only start Queue if we haven't!
-    if(this.queueRunning){
-      return false;
+    // No files were added
+    if (nFiles == 0){
+      return this;
     }
 
-    // and only if new Files were successfully added
-    if(this.queue.length == j){
-      return false;
-    }
-
-    if (this.settings.auto){
+    // Are we auto-uploading files?
+    if (this.settings.auto && this.settings.queue) {
       this.processQueue();
     }
 
-    return true;
+    return this;
   };
 
   DmUploader.prototype.processQueue = function()
   {
+    if (this.queueRunning) {
+      return;
+    }
+
     this.queuePos++;
 
     if(this.queuePos >= this.queue.length){
@@ -424,6 +209,33 @@
     }
 
     this.queue[this.queuePos].upload(this, false);
+  };
+
+  // Public API methods
+  DmUploader.prototype.methods = {
+    start: function(id) {
+      // todo: check auto/queue options
+
+      // todo: check id is present
+
+      return true;
+    },
+    cancel: function(id) {
+      // todo: check auto/queue options
+
+      // todo: check id is present
+
+      return true;
+    },
+    retry: function(id) {
+      // todo: check auto/queue options
+
+      // todo: check id is present
+
+    },
+    reset: function() {
+      return true;
+    }
   };
 
   $.fn.dmUploader = function(options){
