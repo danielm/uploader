348d705a7faf849e5fd20b500cb58155b2a55aae
Daniel Morales <daniel85mg@gmail.com>
2018-01-06 20:57:06 -0300

    Cleaning up the DEMO

---
+++ demo/README.md
@@ -0,0 +1,33 @@
+# DEMO
+
+This page demostrates the most basic setup/config.
+
+## Backend
+
+By default this example uploads files using: <code>demo/backend/upload.php</code>. This piece of PHP logic is provided to upload the files to the <code>backend/files/</code> folder (make sure it has writing permissions).
+
+This is only an example using PHP, of course you can use it with any other type of backend. 
+
+And It's just to demonstrate the flow; the script returns a very basic JSON response  structure (again, this is for this example, you can implement this in any way you want):
+
+### Success response:
+```json
+{
+	status: 'ok',
+	path: 'files/5a515f174dea7_Hot_Gril.jpg'
+}
+```
+
+### Error response:
+```json
+{
+	status: 'error',
+	message: 'Exceeded filesize limit'
+}
+```
+
+# Warning
+
+This is a ***DEMO*** , the backend / PHP provided is very basic. You can use it as a starting point maybe, but ***do not use this on production***. It doesn't preform any server-side validation, checks, authentication, etc.
+
+
--- demo/backend/upload.php
+++ demo/backend/upload.php
@@ -1,6 +1,13 @@
 <?php
 
-// php.net/manual/en/features.file-upload.php
+/*
+  This is a ***DEMO*** , the backend / PHP provided is very basic. You can use it as a starting point maybe, but ***do not use this on production***. It doesn't preform any server-side validation, checks, authentication, etc.
+
+  For more read the README.md file on this folder.
+
+  Based on the examples provided on:
+  - http://php.net/manual/en/features.file-upload.php
+*/
 
 header('Content-type:application/json;charset=utf-8');
 
@@ -24,20 +31,20 @@ try {
             throw new RuntimeException('Unknown errors.');
     }
 
+    $filepath = sprintf('files/%s_%s', uniqid(), $_FILES['file']['name']);
+
     if (!move_uploaded_file(
         $_FILES['file']['tmp_name'],
-        sprintf('./files/%s_%s',
-        	uniqid(),
-        	$_FILES['file']['name']
-        )
+        $filepath
     )) {
         throw new RuntimeException('Failed to move uploaded file.');
     }
 
     // All good, send the response
-    echo json_encode(
-    	['status' => 'ok']
-    );
+    echo json_encode([
+        'status' => 'ok',
+        'path' => $filepath
+    ]);
 
 } catch (RuntimeException $e) {
 	// Something went wrong, send the err message as JSON
